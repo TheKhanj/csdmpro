123f159d3cc032241c0332cafbd84444a1f6fa13
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-03-29 18:04:09 +0330

    add current shit of observer

---
+++ observer.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Observer struct {
+	Repo           *PlayerRepo
+	Crawler        Crawler
+	GotOnline      chan Player
+	GotOffline     chan Player
+	StatsInterval  time.Duration
+	OnlineInterval time.Duration
+
+	shuttingDown bool
+	wg           sync.WaitGroup
+}
+
+func (this *Observer) observeOnlinePlayers() error {
+	players, err := this.Crawler.Online()
+	if err != nil {
+		return err
+	}
+
+	pErr := func(err error) {
+		log.Println(fmt.Errorf("observer: online: %s", err.Error()))
+	}
+
+	for _, player := range players {
+		exists, err := this.Repo.PlayerExists(player.Name)
+		if err != nil {
+			pErr(err)
+			continue
+		}
+		if !exists {
+			err = this.Repo.AddPlayer(player)
+			if err != nil {
+				pErr(err)
+				continue
+			}
+		}
+
+		isOnlineAlready, err := this.Repo.IsOnline(player.Name)
+		if err != nil {
+			pErr(err)
+			continue
+		}
+		if isOnlineAlready {
+			continue
+		}
+
+		playerId, err := this.Repo.GetPlayerId(player.Name)
+		if err != nil {
+			pErr(err)
+			continue
+		}
+		err = this.Repo.AddOnlinePlayer(playerId)
+		if err != nil {
+			pErr(err)
+			continue
+		}
+
+		this.GotOnline <- player
+	}
+
+	prevOnlines, err := this.Repo.Onlines()
+	if err != nil {
+		return err
+	}
+
+	for _, prevOnline := range prevOnlines {
+		isStillOnline := false
+		for _, player := range players {
+			if prevOnline.Name == player.Name {
+				isStillOnline = true
+				break
+			}
+		}
+
+		if isStillOnline {
+			continue
+		}
+
+		playerId, err := this.Repo.GetPlayerId(prevOnline.Name)
+		if err != nil {
+			pErr(err)
+			continue
+		}
+		err = this.Repo.RemoveOnlinePlayer(playerId)
+		if err != nil {
+			pErr(err)
+			continue
+		}
+
+		this.GotOffline <- prevOnline
+	}
+
+	return nil
+}
+
+func (this *Observer) observePlayersPage(page int) error {
+	players, err := this.Crawler.Stats(page)
+	if err != nil {
+		return err
+	}
+
+	for _, player := range players {
+		exists, err := this.Repo.PlayerExists(player.Name)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if exists {
+			continue
+		}
+
+		err = this.Repo.AddPlayer(player)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	return nil
+}
+
+func (this *Observer) observePlayers() {
+	for page := 1; page <= 5; page++ {
+		err := this.observePlayersPage(page)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (this *Observer) Start() {
+	this.wg.Add(2)
+
+	go func() {
+		defer this.wg.Done()
+
+		for {
+			if this.shuttingDown {
+				return
+			}
+
+			err := this.observeOnlinePlayers()
+			if err != nil {
+				log.Println(err)
+			}
+
+			time.Sleep(this.OnlineInterval)
+		}
+	}()
+
+	go func() {
+		defer this.wg.Done()
+
+		for {
+			if this.shuttingDown {
+				return
+			}
+
+			this.observePlayers()
+
+			time.Sleep(this.StatsInterval)
+		}
+	}()
+
+	this.wg.Wait()
+}
+
+func (this *Observer) Stop() {
+	this.shuttingDown = true
+
+	this.wg.Wait()
+}
