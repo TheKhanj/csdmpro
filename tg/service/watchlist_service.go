0863243d494db181d3b52276986b2411e631a043
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-04-02 20:02:08 +0330

    add tg watchlist service

---
+++ tg/service/watchlist_service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"github.com/thekhanj/csdmpro/core"
+	"github.com/thekhanj/csdmpro/tg/repo"
+)
+
+type WatchlistService struct {
+	PlayerRepo    *core.PlayerRepo
+	WatchlistRepo *repo.WatchlistRepo
+}
+
+func (this *WatchlistService) GetTracking(chatId int64) ([]TrackingPlayer, error) {
+	ids, err := this.WatchlistRepo.List(chatId)
+	if err != nil {
+		return nil, err
+	}
+	ret := []TrackingPlayer{}
+	for _, id := range ids {
+		p, err := this.PlayerRepo.GetPlayer(id)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: fix this
+		isOnline, err := this.PlayerRepo.IsOnline(p.Name)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, TrackingPlayer{
+			Player:   p,
+			IsOnline: isOnline,
+		})
+	}
+
+	return ret, nil
+}
+
+type TrackingPlayer struct {
+	Player   core.Player
+	IsOnline bool
+}
